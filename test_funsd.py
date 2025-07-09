826df353663d6e1e17ed35f3741e2dcd07c74ff9
taeyeongkim381 <taeyeongkim381@gmail.com>
2025-07-09 10:57:26 +0000

    Add: FUNSDLayoutLMV1Dataset을 위한 테스트 코드 추가

---
+++ test_funsd.py
@@ -0,0 +1,32 @@
+from transformers import LayoutLMTokenizerFast, LayoutLMModel, LayoutLMConfig
+from datasets import load_dataset
+from torch.utils.data import DataLoader
+from funsd import FUNSDLayoutLMV1Dataset
+
+def main():
+    dataset = load_dataset("nielsr/FUNSD_layoutlmv2")
+    tokenizer = LayoutLMTokenizerFast.from_pretrained("microsoft/layoutlm-base-uncased")
+    config = LayoutLMConfig.from_pretrained("microsoft/layoutlm-base-uncased")
+
+    train_dataset = FUNSDLayoutLMV1Dataset(
+        dataset=dataset["train"],
+        tokenizer=tokenizer,
+        max_length=config.max_position_embeddings
+    )
+    train_loader = DataLoader(train_dataset, batch_size=2)
+
+    model = LayoutLMModel(config)
+    batch = next(iter(train_loader))
+
+    outputs = model(
+        input_ids=batch["input_ids"],
+        attention_mask=batch["attention_mask"],
+        token_type_ids=batch["token_type_ids"],
+        bbox=batch["bboxes"]
+    )
+
+    print(outputs.last_hidden_state.shape)
+
+if __name__ == "__main__":
+    main()
+    
