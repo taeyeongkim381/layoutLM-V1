02056bef564b892830efce7640fede3fa48c875c
taeyeongkim381 <taeyeongkim381@gmail.com>
2025-07-10 03:55:54 +0000

    Update: __all__ 추가

---
+++ dataset/__init__.py
@@ -0,0 +1,8 @@
+from dataset.funsd import FUNSDLayoutLMV1Dataset
+from dataset.sroie import SROIELayoutLMV1Dataset
+from dataset.rvlcdip import RVLCDIPLayoutLMV1Dataset
+
+__all__ = ["FUNSDLayoutLMV1Dataset",
+           "SROIELayoutLMV1Dataset",
+           "RVLCDIPLayoutLMV1Dataset"
+           ]
