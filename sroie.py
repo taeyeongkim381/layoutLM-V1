f0e5475550facd29155cceacc218622bcb31f7a4
taeyeongkim381 <taeyeongkim381@gmail.com>
2025-07-10 00:55:57 +0000

    Add: Initial commit

---
+++ sroie.py
@@ -0,0 +1,70 @@
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+from torchvision.transforms import ToTensor
+
+
+class SROIELayoutLMV1Dataset(Dataset):
+    """
+    bbox는 이미 0~1000으로 normalize되어 있음.
+    """
+    def __init__(self, dataset, tokenizer, max_length=512, pad_token_label_id=-100):
+        self.dataset = dataset
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.pad_token_label_id = pad_token_label_id
+        self.to_tensor = ToTensor()
+    
+    def __len__(self):
+        return len(self.dataset)
+    
+    def __getitem__(self, idx):
+        item = self.dataset[idx]
+
+        words = item["words"]
+        bboxes = item["bboxes"]
+        labels = item["ner_tags"]
+        image = item["images"]
+        image = image.resize((1000, 1000))
+        image_tensor = self.to_tensor(image)
+        fields = item["fields"]
+
+        encoding = self.tokenizer(
+            words,                                # 입력: 단어 리스트 (예: ["Please", "sign", "here"])
+            is_split_into_words=True,             # 입력이 문장이 아니라 단어 단위로 나뉘어져 있음을 명시
+            padding="max_length",                 # max_length까지 패딩 (길이가 짧은 입력을 0으로 채움)
+            truncation=True,                      # max_length 초과 시 뒤쪽 토큰을 잘라냄 (truncation 적용)
+            max_length=self.max_length,           # 토크나이즈된 결과의 최대 길이 (보통 512)
+            return_offsets_mapping=True,          # 각 토큰이 원본 단어에서 어느 위치에 해당하는지 오프셋 정보 반환
+            return_tensors="pt"                   # 결과를 PyTorch 텐서 형태로 반환 (모델 입력용)
+        )
+
+        word_ids = encoding.word_ids(batch_index=0) # 배치의 첫 번째 샘플
+        token_bboxes = []
+        token_labels = []
+        for word_id in word_ids:
+            if word_id is None:
+                token_bboxes.append([0, 0, 0, 0])
+                token_labels.append(self.pad_token_label_id)
+            else:
+                token_bboxes.append(bboxes[word_id])
+                token_labels.append(labels[word_id])
+
+        pad_len = self.max_length - len(token_bboxes)
+        if pad_len > 0:
+            token_bboxes.extend([[0, 0, 0, 0]] * pad_len)
+            token_labels.extend([self.pad_token_label_id] * pad_len)
+
+        token_bboxes = torch.tensor(token_bboxes, dtype=torch.long)
+        token_labels = torch.tensor(token_labels, dtype=torch.long)
+        
+        return {
+            "input_ids": encoding["input_ids"].squeeze(0),
+            "bboxes": token_bboxes,
+            "attention_mask": encoding["attention_mask"].squeeze(0),
+            "token_type_ids": encoding["token_type_ids"].squeeze(0),
+            "labels": token_labels,
+            "image": image_tensor,
+            "fields": fields,
+        }
+    
---
+++ test_sroie.py
@@ -0,0 +1,34 @@
+from transformers import LayoutLMTokenizerFast, LayoutLMModel, LayoutLMConfig
+from datasets import load_dataset
+from torch.utils.data import DataLoader
+from sroie import SROIELayoutLMV1Dataset
+
+def main():
+    dataset = load_dataset("sizhkhy/SROIE")
+    tokenizer = LayoutLMTokenizerFast.from_pretrained("microsoft/layoutlm-base-uncased")
+    config = LayoutLMConfig.from_pretrained("microsoft/layoutlm-base-uncased")
+
+    train_dataset = SROIELayoutLMV1Dataset(
+        dataset=dataset["train"],
+        tokenizer=tokenizer,
+        max_length=config.max_position_embeddings
+    )
+    train_loader = DataLoader(train_dataset, batch_size=2)
+
+    model = LayoutLMModel(config)
+    batch = next(iter(train_loader))
+
+    outputs = model(
+        input_ids=batch["input_ids"],
+        attention_mask=batch["attention_mask"],
+        token_type_ids=batch["token_type_ids"],
+        bbox=batch["bboxes"]
+    )
+
+    print(outputs.last_hidden_state.shape)
+    print(batch['image'].shape)
+    print(batch['labels'].shape)
+
+if __name__ == "__main__":
+    main()
+    
